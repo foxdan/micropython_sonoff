609dcb2f765a8a4e05b30018736df32bdbdcc3c2
foxdan <5571151+foxdan@users.noreply.github.com>
2020-03-10 21:48:49 +0000

    Several updates

--- boot.py
+++ boot.py
@@ -10,33 +10,14 @@ LED = machine.PWM(machine.Pin(13, machine.Pin.OUT), duty=500, freq=2)
 RELAY = machine.Signal(12, machine.Pin.OUT)
 BUTTON = machine.Pin(0, machine.Pin.IN)
 TOGGLED = bytearray((False,))
+REPL = bytearray((False,))
 machine.freq(160000000)
 
 try:
     with open('cfg.json', 'r') as jcfg:
         cfg = ujson.load(jcfg)
 except Exception as e:
-    print(e)
     cfg = dict()
-
-import webrepl
-REPL = bytearray((False,))
-def web_repl(stop=False):
-    if stop:
-        webrepl.stop()
-        REPL[0] = False
-        return
-    if not REPL[0]:
-        webrepl.start(password=cfg.get('DEBUG_PASS', 'python'))
-        REPL[0] = True
-web_repl()
-
-def save_cfg():
-    cfg['HOSTNAME'] = cfg['DEV_TYPE'] + '_' + cfg['DEV_LOCATION']
-    cfg['CLIENT_ID'] = cfg['CLIENT_ID_PREFIX'] + cfg['HOSTNAME']
-    cfg['TOPIC'] = cfg['DEV_TYPE'] + '/' + cfg['DEV_LOCATION']
-    with open('cfg.json', 'w') as jcfg:
-        ujson.dump(cfg, jcfg)
 
 def POWER_ON(publish=False):
     print("POWER ON")
@@ -59,6 +40,31 @@ def button_hdlr(_pin):
     else:
         POWER_ON(publish=True)
     machine.enable_irq(state)
+BUTTON.irq(handler=button_hdlr, trigger=(BUTTON.IRQ_FALLING), hard=True)
+
+def web_repl(stop=False):
+    import webrepl
+    if stop:
+        webrepl.stop()
+        REPL[0] = False
+        return
+    if not REPL[0]:
+        webrepl.start(password=cfg.get('DEBUG_PASS', 'python'))
+        REPL[0] = True
+web_repl()
+
+def save_cfg():
+    with open('cfg.json', 'w') as jcfg:
+        ujson.dump(cfg, jcfg)
+
+def hostname():
+    try:
+        name = '{}_{}_{}'.format(cfg['ROOM'], cfg['TYPE'], cfg['NAME'])
+    except KeyError:
+        import network, ubinascii
+        mac_end = network.WLAN(network.AP_IF).config('mac')[-3:]
+        name = 'pyiot-' + ubinascii.hexlify(mac_end).decode('utf')
+    return name
 
 def fail_mode():
     import network
@@ -69,12 +75,11 @@ def fail_mode():
     LED.freq(3)
     network.WLAN(network.STA_IF).active(False)
     ap.active(True)
-    essid = 'pyiot-' + ubinascii.hexlify(ap.config('mac')[-3:]).decode('utf')
-    ap.config(essid=essid,
+    ap.config(essid=hostname(),
               password='homebrewiot',
               authmode=network.AUTH_WPA2_PSK,
               channel=11)
-    webrepl.start(password='python')
+    web_repl()
 
 def nw_config():
     import network
@@ -83,7 +88,7 @@ def nw_config():
     network.WLAN(network.AP_IF).active(False)
     wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    wlan.config(dhcp_hostname=cfg['HOSTNAME'])
+    wlan.config(dhcp_hostname=hostname())
     wlan.connect(cfg['SSID'], cfg['PASSWORD'])
     timeout = utime.time() + 30
     while True:
@@ -103,7 +108,6 @@ def nw_config():
         elif utime.time() > timeout:
             return False
 
-BUTTON.irq(handler=button_hdlr, trigger=(BUTTON.IRQ_FALLING), hard=True)
 try:
     if not nw_config():
         fail_mode()
--- main.py
+++ main.py
@@ -5,79 +5,93 @@ from umqtt.simple import MQTTClient
 timer = machine.Timer(-1)
 timer_end = 0
 
+def topics(location, room, typ, name):
+    args = (location, room, typ, name)
+    topics = ['/'.join((location, room, typ, name))]
+    for i in range(1, 4):
+        topics.append('/'.join(args[:i]) + '/global')
+    return topics
+
 def callback(topic, msg):
     if not msg:
         return
-    if topic == b'webrepl':
+    _, command = topic.rsplit(b'/', 1)
+    if command == b'webrepl':
         if msg == b'ON':
             print('REPL ON')
             web_repl()
         if msg == b'OFF':
             print('REPL OFF')
             web_repl(stop=True)
-    elif topic.startswith(cfg['TOPIC'].encode('utf')):
-        sub_topic = topic[len(cfg['TOPIC']):].strip(b'/')
-        if not sub_topic:
-            if msg == b'ON':
-                POWER_ON()
-            elif msg == b'OFF':
-                POWER_OFF()
+    elif command == b'set':
+        if msg == b'ON':
+            POWER_ON()
+        elif msg == b'OFF':
+            POWER_OFF()
+    elif command == b'set_timer':
+        period = int(msg) * 3600
+        global timer_end
+        timer_end = utime.time() + period
+        if period > 0:
+            POWER_ON()
+            timer.init(period=period * 1000,
+                       mode=timer.ONE_SHOT,
+                       callback=lambda t: POWER_OFF())
         else:
-            if sub_topic == b'timer/set':
-                period = int(msg) * 3600
-                global timer_end
-                timer_end = utime.time() + period
-                if period > 0:
-                    POWER_ON(publish=True)
-                    timer.init(period=period * 1000,
-                               mode=timer.ONE_SHOT,
-                               callback=lambda t: POWER_OFF(publish=True))
-                else:
-                    timer.deinit()
+            timer.deinit()
 
-client = MQTTClient(cfg['CLIENT_ID'], cfg['MQTT_HOST'], keepalive=60)
+client = MQTTClient(cfg['CLIENT_ID_PREFIX'] + hostname(),
+                    cfg['MQTT_HOST'], keepalive=60)
 client.cb = callback
 
-def connect():
+def connect(clean=False):
     if client.sock:
         client.sock.close()
     while True:
         try:
-            if not client.connect():
-                print("subscribe")
-                client.subscribe(cfg['TOPIC'] + '/#')
-                client.subscribe('webrepl')
+            session_resume = client.connect(clean)
         except Exception as e:
-            print("WTF")
             web_repl()
             print(e)
             utime.sleep(10)
         else:
-            break
+            return session_resume
 
-def publish_state():
+def subscribe(root_topics):
+    for topic in root_topics:
+        client.subscribe(topic+'/set', qos=1)
+        client.subscribe(topic+'/set_timer', qos=1)
+        client.subscribe(topic+'/webrepl', qos=1)
+
+def publish_state(root_topic, toggle=False):
     if RELAY.value():
         msg = b'ON'
     else:
         msg = b'OFF'
-    client.publish(cfg['TOPIC'], msg, retain=True)
+    client.publish(root_topic, msg, retain=True)
+    if toggle:
+        client.publish(root_topic+'/set', msg, retain=True)
 
-def publish_timer():
+def publish_timer(root_topic):
+    topic = root_topic + '/timer'
     remaining = timer_end - utime.time()
     if remaining > 0:
         msg = '{:.2f}'.format(remaining/3600)
     else:
         msg = '0'
-    client.publish(cfg['TOPIC']+'/timer', msg, retain=True)
+    client.publish(topic, msg, retain=True)
 
 def main():
-    connect()
+    mytopics = topics(cfg['LOCATION'],
+                      cfg['ROOM'],
+                      cfg['TYPE'],
+                      cfg['NAME'])
+    root_topic = mytopics[0]
+    connect(clean=True)
+    subscribe(mytopics)
     last_ping = utime.time()
     while True:
         try:
-            if TOGGLED[0]:
-                publish_state()
-                TOGGLED[0] = False
             client.sock.settimeout(5)
             client.wait_msg()
         except OSError as e:
@@ -85,13 +99,16 @@ def main():
                 print("noop")
                 if utime.time() - last_ping > 30:
                     print("ping")
-                    publish_timer()
+                    publish_state(root_topic, toggle=TOGGLED[0])
+                    TOGGLED[0] = False
+                    publish_timer(root_topic)
                     #client.ping()
                     last_ping = utime.time()
             else:
                 print(e)
                 print("reconnect")
-                connect()
+                if not connect(clean=False):
+                    subscribe(mytopics)
 
 try:
     main()
